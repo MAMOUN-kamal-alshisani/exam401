82b77f0700d4a4640506fb2db5c79a2b11f1a0f4
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-08-03 13:18:01 +0300

    one

---
+++ components/Form.js
@@ -0,0 +1,51 @@
+
+
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Component } from 'react';
+
+
+class Update extends Component{
+
+
+  render() {
+    return(
+      <>
+        <h1>My Favorites</h1>
+
+        <Row>
+   
+   
+
+imageUrl
+<form>
+
+<label>Title</label>
+
+<input type='title' value={this.props.title}/>
+
+<label>Title</label>
+
+<input type='title' value={this.props.userName}/>
+
+<label>Title</label>
+
+<input type='title' value={this.props.imageUrl}/>
+
+</form>
+  
+
+
+
+</Row>
+
+
+        <p>
+          This is a collection of my favorites
+        </p>
+      </>
+
+    }
+    }
+
+export default (Update);
--- src/AllDataAPI.js
+++ src/AllDataAPI.js
@@ -1,14 +1,86 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-
-
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card,Row,Button} from 'react-bootstrap'
 class AllDataAPI extends Component {
+constructor(props){
+
+super(props)
+
+this.state={
+
+AllData:[],
+
+
+}
+}
+
+componentDidMount=async()=>{
+    
+let url=`http://localhost:3001/Alldata`
+
+// let url=`${process.env.SERVER_REACT_URL}/Alldata`
+let data =await axios.get(url)
+
+this.setState({
+
+    AllData:data.data
+
+})
+console.log(data.data)
+}
+
+AddToFav=async(el)=>{
+
+const reqbody={
+
+    title:el.title,
+    userName:el.userName,
+    imageUrl:el.imageUrl,
+}
+
+let url=`http://localhost:3001/AddData?email${this.props.auth0.user.email}`
+
+await axios.post(url,reqbody)
+
+}
+
+
+
     render() {
         return (
-            <div>
+            <>
                 <h1>All Data from the API</h1>
+                
+<Row>
+   
+    {
+
+this.state.AllData.map((el,idx)=>{
+    return(
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={el.imageUrl} alt={el.title} />
+    <Card.Body>
+      <Card.Title>{el.title}</Card.Title>
+      <Card.Text>
+     {el.userName}
+      </Card.Text>
+      <Button variant="primary" onClick={()=>this.AddToFav}>add to favorite</Button>
+    </Card.Body>
+  </Card>
+    )
+
+
+
+})
+
+    }
+  
+
+</Row>
                 <h3>Select your favorites :)</h3>
-            </div>
+            </>
         )
     }
 }
